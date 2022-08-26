736fe217416e156601ad8fe87e5081bdc5b91e54
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-08-26 13:44:55 +0700

    Add files via upload

---
+++ kode2.-29-1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+  char key[] = "karenabelajar.blogspot.com";
+  char buffer[80];
+  do {
+     printf ("Apa nama website ini? ");
+     fflush (stdout);
+     scanf ("%79s",buffer);
+  } while (strcmp (key,buffer) != 0);
+  puts ("jawaban benar!");
+  return 0;
+}
